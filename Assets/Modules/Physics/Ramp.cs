b802e38576ab70f1b389ca03137029b99fd516b7
Miłosz Klim <klim.milosz@gmail.com>
2023-08-26 15:52:28 +0200

    Poke UI and classroom fixes

--- Assets/Modules/Physics/Ramp.cs
+++ Assets/Modules/Physics/Ramp.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -6,6 +7,7 @@ using UnityEngine;
 public class Ramp : MonoBehaviour
 {
     [SerializeField] private Rigidbody ramp;
+    [SerializeField] private float debugRampRotation;
     [Header("Materials")]
     [SerializeField] private PhysicMaterial rampMaterial;
     [SerializeField] private PhysicMaterial blockMaterial;
@@ -15,6 +17,7 @@ public class Ramp : MonoBehaviour
     [SerializeField] private TextMeshProUGUI staticBlock;
     [SerializeField] private TextMeshProUGUI dynamicRamp;
     [SerializeField] private TextMeshProUGUI dynamicBlock;
+    
 
     private void Start()
     {
@@ -24,28 +27,37 @@ public class Ramp : MonoBehaviour
         if (dynamicRamp != null) dynamicRamp.SetText($"{rampMaterial.dynamicFriction}");
         if (dynamicBlock != null) dynamicBlock.SetText($"{blockMaterial.dynamicFriction}");
     }
+    [ContextMenu("UpdateRampAxis")]
+    public void UpdateRampAxis()
+    {
+        MoveRamp(debugRampRotation);
+    }
     public void MoveRamp(float angle)
     {
-        ramp.MoveRotation(Quaternion.Euler(angle, 0,0));
+        ramp.MoveRotation(Quaternion.Euler(angle, ramp.transform.eulerAngles.y, ramp.transform.eulerAngles.z));
         if (angleOut != null) angleOut.SetText($"{angle}");
     }
     public void SetStaticRamp(float value)
     {
+        value = (float)Math.Round(value,2);
         rampMaterial.staticFriction = value;
         if(staticRamp != null) staticRamp.SetText($"{value}");
     }
     public void SetStaticBlock(float value)
     {
+        value = (float)Math.Round(value, 2);
         blockMaterial.staticFriction = value;
         if (staticBlock != null) staticBlock.SetText($"{value}");
     }
     public void SetDynamicRamp(float value)
     {
+        value = (float)Math.Round(value, 2);
         rampMaterial.dynamicFriction = value;
         if (dynamicRamp != null) dynamicRamp.SetText($"{value}");
     }
     public void SetDynamicBlock(float value)
     {
+        value = (float)Math.Round(value, 2);
         blockMaterial.dynamicFriction = value;
         if (dynamicBlock != null) dynamicBlock.SetText($"{value}");
     }
