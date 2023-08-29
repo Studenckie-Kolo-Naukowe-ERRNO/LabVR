1c7fa04fa2c0c84451e33e52e372cd474ab9f7b4
Miłosz Klim <klim.milosz@gmail.com>
2023-08-29 14:10:33 +0200

    Hub and async level loading system

--- Assets/Scripts/ErrnoCharacterController.cs
+++ Assets/Scripts/ErrnoCharacterController.cs
@@ -5,27 +5,42 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit;
 
 [RequireComponent(typeof(CharacterController))]
 public class ErrnoCharacterController : MonoBehaviour
 {
     private CharacterController characterController;
+    private ActionBasedContinuousMoveProvider moveProvider;
+    [SerializeField] private Camera mainCam;
+    [Header("Stats")]
+    [SerializeField] private float moveSpeed = 1;
     [Header("Loading")]
     [SerializeField] private Image loadingImage;
     [SerializeField] private float fadeTime = 1;
     void Start()
     {
         characterController = GetComponent<CharacterController>();
-        StartCoroutine(FadeInOut(false));
+        moveProvider = GetComponent<ActionBasedContinuousMoveProvider>();
+        if(moveProvider != null)
+        {
+            moveProvider.moveSpeed = moveSpeed;
+        }
+        FadeOut();
     }
 
+    //true means screen getting darker
     IEnumerator FadeInOut(bool fadeIn)
     {
         float alpha = fadeIn ? 0f : 1f;
         Color fadeColor = new Color(0, 0, 0, alpha);
 
         loadingImage.color = fadeColor;
-        if(!fadeIn) yield return new WaitForSeconds(1);
+
+        yield return null;
+        mainCam.enabled = true;
+
+        if (!fadeIn) yield return new WaitForSeconds(1);
         while (alpha>=0 && alpha<=1)
         {
             if (fadeIn) alpha += Time.deltaTime / fadeTime;
@@ -50,5 +65,15 @@ public class ErrnoCharacterController : MonoBehaviour
     {
         StopAllCoroutines();
         StartCoroutine(FadeInOut(true));
+    }
+
+    public float GetFadeTime()
+    {
+        return fadeTime;
+    }
+    //true means move is locked
+    public void LockMovement(bool lockState)
+    {
+        moveProvider.moveSpeed = lockState ? 0 : moveSpeed;
     }
 }
