436b5784f2ba1767483c42699de9ee6c9ed8e5e8
Cosinus215 <oliwka_pawlowska@wp.pl>
2023-09-03 23:36:08 +0200

    Add mining rock when hit with a tool script

---
+++ Assets/VRLab/Scripts/MineRockOnHit.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GeoLab {
+    public class MineRockOnHit : MonoBehaviour {
+        [SerializeField] private float health = 1000;
+        [SerializeField] private GameObject[] smallRocksPrefabs;
+        private List<GameObject> smallRocksGameobject = new List<GameObject>();
+
+        private void Start() {
+            LoadSmallRocksOnScene();
+        }
+
+        private void OnCollisionEnter(Collision collision) {
+            if (collision.gameObject.layer == 8) {
+                Vector3 collisionForce = collision.impulse / Time.fixedDeltaTime;
+                health -= collisionForce.magnitude;
+
+                if (health <= 0) {
+                    DestroyTheRock();
+                }
+            }
+        }
+
+        private void OnDestroy() {
+            CreateSmallRocks();
+        }
+
+        private void LoadSmallRocksOnScene() {
+            foreach (GameObject smallRock in smallRocksPrefabs) {
+                GameObject rock = Instantiate(smallRock);
+                rock.SetActive(false);
+                rock.transform.localScale = new Vector3(0.008f, 0.008f, 0.008f);
+                smallRocksGameobject.Add(rock);
+            }
+        }
+
+        private void DestroyTheRock() {
+            Destroy(gameObject);
+            return;
+        }
+
+        private void CreateSmallRocks() {
+            gameObject.GetComponent<MeshCollider>().enabled = false;
+            foreach (GameObject smallRock in smallRocksGameobject) {
+                smallRock.transform.position = transform.position + new Vector3(0, 0.8f);
+                smallRock.SetActive(true);
+            }
+        }
+    }
+}
