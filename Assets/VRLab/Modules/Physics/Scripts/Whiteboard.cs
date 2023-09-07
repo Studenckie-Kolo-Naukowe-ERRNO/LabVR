bdaccb31dd1271f854f3d643bd793bd7f031fbbf
Miłosz Klim <klim.milosz@gmail.com>
2023-09-07 16:25:07 +0200

    Better board

--- Assets/VRLab/Modules/Physics/Scripts/Whiteboard.cs
+++ Assets/VRLab/Modules/Physics/Scripts/Whiteboard.cs
@@ -6,25 +6,42 @@ namespace PhysicsLab
 {
     public class Whiteboard : MonoBehaviour
     {
-        private Material drawingMaterial;
-        [SerializeField] private RenderTexture drawingRenderTexture;
         [SerializeField] private Camera drawingCamera;
-        public bool checkForMarker = false;
+        [SerializeField] private GameObject whiteboardCanvas;
+
+        [SerializeField] private Transform leftCorner;
+        [SerializeField] private Transform rightCorner;
+
+        private Material drawingMaterial;
+        private RenderTexture drawingRenderTexture;
+        private bool calculateMarkers = false;
+        private int markerCount = 0;
+
+        private const int RESOLUTION = 512;
 
         void Start()
         {
-            drawingMaterial = GetComponent<Renderer>().material;
-            drawingRenderTexture = new RenderTexture(512, 256, 8, RenderTextureFormat.ARGB32);
+            drawingMaterial = whiteboardCanvas.GetComponent<Renderer>().material;
+
+            //float canvasRealSizeX = Mathf.Abs(leftCorner.localPosition.x - rightCorner.localPosition.x) * whiteboardCanvas.transform.localScale.x;
+            //float canvasRealSizeY = Mathf.Abs(leftCorner.localPosition.z - rightCorner.localPosition.z) * whiteboardCanvas.transform.localScale.z;
+
+            float res = (whiteboardCanvas.transform.localScale.x * transform.localScale.x)
+                / (whiteboardCanvas.transform.localScale.z * transform.localScale.y);
+
+            drawingRenderTexture = new RenderTexture((int)(RESOLUTION* res), RESOLUTION, 8, RenderTextureFormat.ARGB32);
+
             drawingMaterial.SetTexture("_BaseMap", drawingRenderTexture);
+
             drawingCamera.enabled = false;
             drawingCamera.targetTexture = drawingRenderTexture;
-            drawingCamera.orthographicSize = 1.5f;
-            drawingCamera.farClipPlane = 0.18f;
-            drawingCamera.nearClipPlane = 0.09f;
+            
+            float distance = Mathf.Abs(leftCorner.transform.position.y - rightCorner.transform.position.y)/2;
+            drawingCamera.orthographicSize = distance;
         }
         private void Update()
         {
-            if(checkForMarker)
+            if(calculateMarkers)
             {
                 drawingCamera.Render();
             }
@@ -33,17 +50,19 @@ namespace PhysicsLab
 
         private void OnTriggerEnter(Collider other)
         {
-            if (other.tag == "Marker")
+            if (other.CompareTag("Marker"))
             {
-                checkForMarker = true;
+                markerCount++;
+                calculateMarkers = (markerCount>0);
             }
         }
 
         private void OnTriggerExit(Collider other)
         {
-            if(other.tag == "Marker")
+            if(other.CompareTag("Marker"))
             {
-                checkForMarker = false;
+                markerCount--;
+                calculateMarkers = (markerCount > 0);
             }
         }
     }
