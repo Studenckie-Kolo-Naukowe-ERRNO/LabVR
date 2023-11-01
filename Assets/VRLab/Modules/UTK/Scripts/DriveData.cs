c99f236acf923a202fdf579a66a7807a67e03ce3
Miłosz Klim <71337890+DAXPL@users.noreply.github.com>
2023-11-01 19:48:34 +0100

    Fixes

--- Assets/VRLab/Modules/UTK/Scripts/DriveData.cs
+++ Assets/VRLab/Modules/UTK/Scripts/DriveData.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-namespace VRLabEssentials{
-    public class HardDrive : MonoBehaviour
+
+namespace UTKLab
+{
+    [CreateAssetMenu(menuName = "Components/Disc")]
+    public class DriveData : ScriptableObject
     {
         public enum DriveInterface
         {
@@ -22,13 +25,13 @@ namespace VRLabEssentials{
 
         [Header("Disk type data")]
         [Tooltip("Select disk type")]
-        [SerializeField]private DriveType driveType;
+        [SerializeField] private DriveType driveType;
         [Tooltip("Is disk external?")]
-        [SerializeField]private bool isExternal;
+        [SerializeField] private bool isExternal;
 
         [Header("General disk settings")]
         [Tooltip("Select disk size in megabytes")]
-        [SerializeField]private int totalDiskSpace;
+        [SerializeField] private int totalDiskSpace;
         private int usedDiskSpace;
         [Tooltip("Select transfer rate in megabytes/second")]
         [SerializeField] private int readSpeed;
@@ -39,64 +42,64 @@ namespace VRLabEssentials{
 
         [Header("HDD data")]
         [Tooltip("Select disk's rotations per minute")]
-        [SerializeField]private int rotationsPerMinute;
-        [Tooltip("Select disk's rotations per minute")]
-        [SerializeField]private DriveInterface driveInterface;
+        [SerializeField] private int rotationsPerMinute;
+        [Tooltip("Select disk's rotations per minute")] 
+        [SerializeField] private DriveInterface driveInterface;
 
         public DriveType GetDriveType()
         {
             return driveType;
         }
+
         public bool GetIsExternal()
         {
             return isExternal;
         }
+
         public int GetTotalDiskSpace()
         {
             return totalDiskSpace;
         }
+
         public int GetUsedDiskSpace()
         {
             return usedDiskSpace;
         }
+
         public int GetRemainingDiskSpace()
         {
-            return totalDiskSpace-usedDiskSpace;
+            return totalDiskSpace - usedDiskSpace;
         }
+
         public int GetReadSpeed()
         {
             return readSpeed;
         }
+
         public int GetWriteSpeed()
         {
             return writeSpeed;
         }
+
         public int GetInterfaceSpeed()
         {
             return interfaceSpeed;
         }
+
         public int GetRotationsPerMinute()
         {
             return rotationsPerMinute;
         }
+
         public DriveInterface GetInterface()
         {
             return driveInterface;
         }
+
         public void SetUsedDiskSpace(int usedDiskSpace)
         {
             this.usedDiskSpace = usedDiskSpace;
         }
 
-        public void SetHardDrive(DriveType driveType, bool isExternal, int totalDiskSpace, int readSpeed, int writeSpeed, int interfaceSpeed, DriveInterface driveInterface){
-            this.driveType = driveType;
-            this.isExternal = isExternal;
-            this.totalDiskSpace = totalDiskSpace;
-            this.usedDiskSpace = 0;
-            this.readSpeed = readSpeed;
-            this.writeSpeed = writeSpeed;
-            this.interfaceSpeed = interfaceSpeed;
-            this.driveInterface = driveInterface;
-        }
     }
 }
