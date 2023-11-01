b37b0c08f085da41044e016c84dc039d02cd5c43
Miłosz Klim <71337890+DAXPL@users.noreply.github.com>
2023-11-01 20:15:07 +0100

    Fixes

---
+++ Assets/VRLab/Modules/UTK/Scripts/GPU.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UTKLab;
+
+public class GPU : MonoBehaviour
+{
+    [SerializeField] private GPUData data;
+}
---
+++ Assets/VRLab/Modules/UTK/Scripts/GPUArchitecture.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UTKLab
+{
+    [CreateAssetMenu(menuName = "Components/GPU Arch")]
+    public class GPUArchitecture : ScriptableObject
+    {
+        [SerializeField] private string architectureName = "Pascal <3";
+        [SerializeField] private float TFLOPScore = 769.4f; // wynik benchamarka za 1 TFLOP (Na podstawie 3D Mark - Time Spy (DX12) wynik rtx 3060 (12,7 TFLOP = 9801) )
+    
+        public string GetArchName()
+        {
+            return architectureName;
+        }
+        public float GetTFLOPScore()
+        {
+            return TFLOPScore;
+        }
+    }
+}
