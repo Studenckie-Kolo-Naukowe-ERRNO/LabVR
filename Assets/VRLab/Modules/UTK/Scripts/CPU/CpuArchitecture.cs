8c8d419b646e13aa720f80d74207ad766aca23bd
Cosinus215 <oliwka_pawlowska@wp.pl>
2023-11-03 19:06:47 +0100

    Fix namespaces and ScriptableObject menuName

--- Assets/VRLab/Modules/UTK/Scripts/CPU/CpuArchitecture.cs
+++ Assets/VRLab/Modules/UTK/Scripts/CPU/CpuArchitecture.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Cpu Architecture", menuName = "ScriptableObjects/New Cpu Architecture")]
-public class CpuArchitecture : ScriptableObject {
+namespace UTKLab {
+    [CreateAssetMenu(fileName = "New Cpu Architecture", menuName = "ScriptableObjects/Components/Cpu Architecture")]
+    public class CpuArchitecture : ScriptableObject {
 
-    public string cpuBrand;
-    public string cpuArchitectureName;
-    public int ipc;
-    public float multiThreadMultipiler;
+        public string cpuBrand;
+        public string cpuArchitectureName;
+        public int ipc;
+        public float multiThreadMultipiler;
+    }
 }
--- Assets/VRLab/Modules/UTK/Scripts/CPU/CpuSocket.cs
+++ Assets/VRLab/Modules/UTK/Scripts/CPU/CpuSocket.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Socket", menuName = "ScriptableObjects/New Socket")]
-public class CpuSocket : ScriptableObject {
-    public string socketName;
+namespace UTKLab {
+    [CreateAssetMenu(fileName = "New Socket", menuName = "ScriptableObjects/Components/Socket")]
+    public class CpuSocket : ScriptableObject {
+        public string socketName;
+    }
 }
