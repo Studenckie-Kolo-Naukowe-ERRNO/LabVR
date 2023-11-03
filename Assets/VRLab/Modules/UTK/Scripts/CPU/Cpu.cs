f6de77fd1f4c8ec0c0fa07ab4cfac59a5a7b6389
Cosinus215 <oliwka_pawlowska@wp.pl>
2023-11-03 01:17:06 +0100

    Add UTKLab namespace

--- Assets/VRLab/Modules/UTK/Scripts/CPU/Cpu.cs
+++ Assets/VRLab/Modules/UTK/Scripts/CPU/Cpu.cs
@@ -2,30 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cpu : MonoBehaviour { 
+namespace UTKLab {
+    public class Cpu : MonoBehaviour {
 
-    [SerializeField] private CpuArchitecture cpuArchitecture;
-    [SerializeField] private CpuSocket cpuSocket;
-    [SerializeField] private string model;
-    [SerializeField] private float clockRate;
-    [SerializeField] private int cores;
-    [SerializeField] private int threads;
-    [SerializeField] private bool unlockedMultiplier;
-    [SerializeField] private bool integratedGraphics;
-    [SerializeField] private int TDP;
+        [SerializeField] private CpuArchitecture cpuArchitecture;
+        [SerializeField] private CpuSocket cpuSocket;
+        [SerializeField] private string model;
+        [SerializeField] private float clockRate;
+        [SerializeField] private int cores;
+        [SerializeField] private int threads;
+        [SerializeField] private bool unlockedMultiplier;
+        [SerializeField] private bool integratedGraphics;
+        [SerializeField] private int TDP;
 
-    [ContextMenu("PrintPerformance")]
-    public void PrintPerformance() {
-        Debug.Log(SingleThreadPerformance());
-        Debug.Log(MultiThreadPerformance());
+        [ContextMenu("PrintPerformance")]
+        public void PrintPerformance() {
+            Debug.Log(SingleThreadPerformance());
+            Debug.Log(MultiThreadPerformance());
+        }
+
+        private float SingleThreadPerformance() {
+            return clockRate * cpuArchitecture.ipc;
+        }
+
+        private float MultiThreadPerformance() {
+            return clockRate * cpuArchitecture.ipc * threads * cpuArchitecture.multiThreadMultipiler;
+        }
+
     }
-
-    private float SingleThreadPerformance() {
-        return clockRate * cpuArchitecture.ipc;
-    }
-
-    private float MultiThreadPerformance() {
-        return clockRate * cpuArchitecture.ipc * threads * cpuArchitecture.multiThreadMultipiler;
-    }
-
 }
