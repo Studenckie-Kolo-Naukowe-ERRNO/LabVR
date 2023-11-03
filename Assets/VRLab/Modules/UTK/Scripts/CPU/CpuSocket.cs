20acad042fd8fb43680c1fcef1f049cdfac287ac
Cosinus215 <oliwka_pawlowska@wp.pl>
2023-11-03 01:13:32 +0100

    Add CPU socket ScriptableObject script

---
+++ Assets/VRLab/Modules/UTK/Scripts/CPU/CpuSocket.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Socket", menuName = "ScriptableObjects/New Socket")]
+public class CpuSocket : ScriptableObject {
+    public string socketName;
+}
