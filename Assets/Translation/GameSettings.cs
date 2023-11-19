bb4cfd20d0ff00e9e4a8b09cfd3408d33218427c
Miłosz Klim <71337890+DAXPL@users.noreply.github.com>
2023-11-19 11:30:38 +0100

    Fixed Hermetization and namespaces

--- Assets/Translation/GameSettings.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "new GameSettings", menuName = "ScriptableObjects/new GameSettings")]
-public class GameSettings : ScriptableObject {
-    public string language;
-}
