6c741912f8bb376263575e41bbca3087af07d5ca
Cosinus215 <oliwka_pawlowska@wp.pl>
2023-12-03 19:04:47 +0100

    Add music and effects volume change

--- Assets/Tablet.cs
+++ Assets/Tablet.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class Tablet : MonoBehaviour {
+    [SerializeField] private AudioMixer effectsVolume;
+    [SerializeField] private AudioMixer musicVolume;
 
     [ContextMenu("Restart The Level")]
     public void RestartTheLevel() {
@@ -19,5 +22,13 @@ public class Tablet : MonoBehaviour {
     public void ChangeGraphicLevel(int level) {
         level = Mathf.Clamp(level, 0, QualitySettings.names.Length - 1);
         QualitySettings.SetQualityLevel(level, true);
+    }
+
+    public void SetEffectVolume(float sliderValue) {
+        effectsVolume.SetFloat("EffectsVolume", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void SetMusicVolume(float sliderValue) {
+        musicVolume.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
     }
 }
