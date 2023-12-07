680e21fc433225d674e85cc00661cc9cc242fea2
Cosinus215 <oliwka_pawlowska@wp.pl>
2023-12-08 00:11:10 +0100

    Add basic opening setting animation

--- Assets/OpenSetting.cs
+++ Assets/OpenSetting.cs
@@ -1,16 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OpenSetting : MonoBehaviour {
-
-    private void Start() {
-        
-    }
+    public GridLayoutGroup gridLayoutGroup;
+    public float duration;
+    public float duration2;
+    public Vector2 startingSize;
+    public Vector2 startingPos;
 
     public void OpenTab(GameObject setting) {
-        setting.SetActive(true);
+        startingPos = setting.GetComponent<RectTransform>().anchoredPosition;
+        gridLayoutGroup.enabled = false;
+        StartCoroutine(OpenSettingAnimation(setting));
+    }
 
-        transform.parent.gameObject.SetActive(false);
+    public void CloseTab(GameObject setting) {
+        StartCoroutine(CloseSettingAnimation(setting));
+    }
+
+    IEnumerator OpenSettingAnimation(GameObject setting) {
+        Vector2 originalSize = setting.GetComponent<RectTransform>().sizeDelta;
+        Vector2 targetSize = GetComponent<RectTransform>().sizeDelta;
+
+        float startTime = Time.time;
+
+        while (setting.GetComponent<RectTransform>().sizeDelta != targetSize) {
+            float progress = (Time.time - startTime) / duration;
+
+            setting.GetComponent<RectTransform>().sizeDelta = Vector2.Lerp(originalSize, targetSize, progress);
+
+            setting.GetComponent<RectTransform>().localPosition = Vector2.Lerp(setting.GetComponent<RectTransform>().localPosition, Vector2.zero, duration2);
+
+            yield return null;
+        }
+
+        setting.GetComponent<RectTransform>().localPosition = Vector2.zero;
+    }
+
+    IEnumerator CloseSettingAnimation(GameObject setting) {
+        Vector2 originalSize = setting.GetComponent<RectTransform>().sizeDelta;
+        Vector2 targetSize = startingSize;
+
+        float startTime = Time.time;
+
+        while (setting.GetComponent<RectTransform>().sizeDelta != targetSize) {
+            float progress = (Time.time - startTime) / duration;
+            float progress2 = (Time.time - startTime) / duration2;
+
+            setting.GetComponent<RectTransform>().sizeDelta = Vector2.Lerp(originalSize, targetSize, progress);
+
+            setting.GetComponent<RectTransform>().anchoredPosition = Vector2.Lerp(setting.GetComponent<RectTransform>().anchoredPosition, startingPos, duration2);
+
+            yield return null;
+        }
+
+        setting.GetComponent<RectTransform>().anchoredPosition = startingPos;
+
+        gridLayoutGroup.enabled = true;
+
     }
 }
+
