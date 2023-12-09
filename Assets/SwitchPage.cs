db68bdce9ae13b1283e889b2c4a76af6bda32873
Cosinus215 <oliwka_pawlowska@wp.pl>
2023-12-10 00:45:25 +0100

    Add swapping to the left

--- Assets/SwitchPage.cs
+++ Assets/SwitchPage.cs
@@ -6,12 +6,14 @@ using UnityEngine;
 public class SwitchPage : MonoBehaviour {
     [SerializeField] private GameObject[] pages;
     private int currentPage = 0;
-    private Vector2 nextPageStartingPos;
+    private Vector2 nextPageStartingPosRight;
+    private Vector2 nextPageStartingPosLeft;
     [SerializeField] private GameObject arrowLeft;
     [SerializeField] private GameObject arrowRight;
 
     private void Start() {
-        nextPageStartingPos = new Vector2(GetComponent<RectTransform>().sizeDelta.x, 0);
+        nextPageStartingPosRight = new Vector2(GetComponent<RectTransform>().sizeDelta.x, 0);
+        nextPageStartingPosLeft = new Vector2(-GetComponent<RectTransform>().sizeDelta.x, 0);
 
         if (pages.Length == 0 || pages.Length == 1) {
             Debug.LogWarning("No pages added");
@@ -23,7 +25,8 @@ public class SwitchPage : MonoBehaviour {
     public void SwitchRight() {
         currentPage++;
         if (currentPage >= pages.Length) currentPage = 0;
-        SwapAnimation();
+        pages[currentPage].GetComponent<RectTransform>().anchoredPosition = nextPageStartingPosRight;
+        StartCoroutine(SwapPageRight());
         pages[currentPage].SetActive(true);
     }       
     
@@ -31,15 +34,12 @@ public class SwitchPage : MonoBehaviour {
         //pages[currentPage].SetActive(false);
         currentPage--;
         if (currentPage < 0) currentPage = pages.Length-1;
+        pages[currentPage].GetComponent<RectTransform>().anchoredPosition = nextPageStartingPosLeft;
+        StartCoroutine(SwapPageLeft());
         pages[currentPage].SetActive(true);
     }
 
-    private void SwapAnimation() {
-        pages[currentPage].GetComponent<RectTransform>().anchoredPosition = nextPageStartingPos;
-        StartCoroutine(SwapPage());
-    }
-
-    IEnumerator SwapPage() {
+    IEnumerator SwapPageRight() {
         while (pages[currentPage].GetComponent<RectTransform>().anchoredPosition.x >= 0) {
             yield return new WaitForSeconds(0.01f);
             foreach (GameObject page in pages) {
@@ -49,4 +49,16 @@ public class SwitchPage : MonoBehaviour {
 
         pages[currentPage].GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
     }
+
+    IEnumerator SwapPageLeft() {
+        while (pages[currentPage].GetComponent<RectTransform>().anchoredPosition.x <= 0) {
+            yield return new WaitForSeconds(0.01f);
+            foreach (GameObject page in pages) {
+                page.transform.position += new Vector3(10, 0, 0);
+            }
+        }
+
+        pages[currentPage].GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+    }
 }
+
